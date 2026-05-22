166950866e57a3c09d0574be58f40b1d6561fc99
vanTruong179 <vantruong1305.vn@gmail.com>
2026-05-22 16:21:21 +0700

    chore(frontend): add optimized Docker builds

--- frontend/apps/moderator/next.config.js
+++ frontend/apps/moderator/next.config.js
@@ -1,6 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, '../../..')
+  },
+  productionBrowserSourceMaps: false
 };
 
 module.exports = nextConfig;
